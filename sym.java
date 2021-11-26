479570f759fedda33a92b2c9b2ba3923d2c73453
matetebra <95086606+matetebra@users.noreply.github.com>
2021-11-26 14:33:57 +0100

    Add files via upload

---
+++ sym.java
@@ -0,0 +1,31 @@
+
+public class sym {
+
+	public final static int EOF = 0;
+	public final static int PROGRAM = 1;
+	public final static int VAR = 2;
+	public final static int INTEGER = 3;
+	public final static int CHAR = 4;
+	public final static int BEGIN = 5;
+	public final static int END = 6;
+	public final static int READ = 7;
+	public final static int WRITE = 8;
+	public final static int ID = 9;
+	public final static int CONST = 10;
+	public final static int PLUS = 11;
+        public final static int MINUS = 12;
+	public final static int LEFTPAR = 13;
+	public final static int RIGHTPAR = 14;
+	public final static int ASSIGN = 15;
+	public final static int SEMICOLON = 16;
+	public final static int COMMA = 17;
+	public final static int DOT = 18;
+	public final static int COLON = 19;
+        public final static int STRING = 20;
+        public final static int OPEN = 21;
+        public final static int IN = 22;
+        public final static int DO = 23;
+        public final static int RETURN = 24;
+        public final static int FILE = 25;
+
+}
